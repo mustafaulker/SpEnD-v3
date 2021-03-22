d1870cbe24b1c5ca55dd631118830763fbeb63e5
mustafaulker <mcubjk@gmail.com>
2021-03-22 16:01:43 +0300

    Improve filters.js
    search_filter and alive_filter integrated with each other. For example, live endpoints can be re-filtered in search filter result.

--- src/frontend/static/js/filters.js
+++ src/frontend/static/js/filters.js
@@ -1,18 +1,35 @@
 function search_filter() {
-    let input, filter, table, tr, td, i, txtValue
+    let input, filter, table, tr, td, i, txtValue, checkBox, alives, td2, txtValue2
+
     input = document.getElementById("endpointSearch")
     filter = input.value.toUpperCase()
     table = document.getElementById("endpointTable")
     tr = table.getElementsByTagName("tr")
+    checkBox = document.getElementById("aliveCheckbox")
 
-    for (i = 0; i < tr.length; i++) {
-        td = tr[i].getElementsByTagName("td")[1]
-        if (td) {
-            txtValue = td.textContent || td.innerText;
-            if (txtValue.toUpperCase().indexOf(filter) > -1) {
-                tr[i].style.display = ""
-            } else {
-                tr[i].style.display = "none"
+    if (checkBox.checked) {
+        alives = alive_filter()
+        for (i = 0; i < alives.length; i++) {
+            td2 = alives[i].getElementsByTagName("td")[1]
+            if (td2) {
+                txtValue2 = td2.textContent || td2.innerText;
+                if (txtValue2.toUpperCase().indexOf(filter) > -1) {
+                    alives[i].style.display = ""
+                } else {
+                    alives[i].style.display = "none"
+                }
+            }
+        }
+    } else {
+        for (i = 0; i < tr.length; i++) {
+            td = tr[i].getElementsByTagName("td")[1]
+            if (td) {
+                txtValue = td.textContent || td.innerText;
+                if (txtValue.toUpperCase().indexOf(filter) > -1) {
+                    tr[i].style.display = ""
+                } else {
+                    tr[i].style.display = "none"
+                }
             }
         }
     }
@@ -20,6 +37,8 @@ function search_filter() {
 
 function alive_filter() {
     let table, tr, td, i, txtValue, checkBox
+    let alive_list = []
+
     table = document.getElementById("endpointTable")
     tr = table.getElementsByTagName("tr")
     checkBox = document.getElementById("aliveCheckbox")
@@ -31,14 +50,14 @@ function alive_filter() {
                 txtValue = td.textContent || td.innerText
                 if (txtValue === "True") {
                     tr[i].style.display = ""
+                    alive_list.push(tr[i])
                 } else {
                     tr[i].style.display = "none"
                 }
             }
         }
+        return alive_list
     } else {
-        for (i = 0; i < tr.length; i++) {
-            tr[i].style.display = ""
-        }
+        search_filter()
     }
 }
