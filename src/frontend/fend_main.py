3fc392eddc52bc9c158d96c8eb2c09cae6cbb984
mustafaulker <mcubjk@gmail.com>
2021-03-23 21:16:43 +0300

    Create query, about, contact pages
    Navbar pages created. Page contents written except query page.
    
    * Add routes to new pages.
    * Add hover to endpoint table.
    * Edit navbar items. Some items merged on a page.

--- src/frontend/fend_main.py
+++ src/frontend/fend_main.py
@@ -26,5 +26,20 @@ def index():
     return render_template('index.html', endpoints=endpoints)
 
 
+@app.route('/query.html')
+def query():
+    return render_template('query.html')
+
+
+@app.route('/about.html')
+def about():
+    return render_template('about.html')
+
+
+@app.route('/contact.html')
+def contact():
+    return render_template('contact.html')
+
+
 if __name__ == "__main__":
     app.run(debug=True)
