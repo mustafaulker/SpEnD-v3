7642b0223e10ada6b4aa303724c59201dedd7138
mustafaulker <mcubjk@gmail.com>
2021-06-03 12:36:19 +0300

    Feature submenu auto-toggle based on the sublink
    * Reduce truncate limit

--- src/frontend/static/js/accordion-toggler.js
+++ src/frontend/static/js/accordion-toggler.js
@@ -1,9 +1,19 @@
 $("#leftside-navigation .sub-menu > a").click(function (e) {
     $("#leftside-navigation ul ul").slideUp();
-    $(this).next().is(":visible") || $(this).next().slideDown();
+    $(this).next().is(":visible") || $(this).next().slideDown()
 
-    $(this).find("i.arrow").toggleClass("toogle");
+    $(this).find("i.arrow").toggleClass("toogle")
     $("#leftside-navigation a").not(this).find("i.arrow").removeClass("toogle")
 
     e.stopPropagation()
 })
+
+$('.sub-menu').each(function () {
+    let $dropdownmenu = $(this)
+    $(this).find('li').each(function () {
+        if ($(this).find('a').attr('href') === location.pathname) {
+            $dropdownmenu.find('ul').is(":visible") || $dropdownmenu.find('ul').slideDown()
+            $dropdownmenu.find("i.arrow").toggleClass("toogle")
+        }
+    });
+});
